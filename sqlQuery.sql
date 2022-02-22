7c6d6ef650c6986631f996fcabc93fe537359656
developerofshadows <46842971+developerofshadows@users.noreply.github.com>
2022-02-23 01:29:48 +0300

    Update sqlQuery.sql

--- sqlQuery.sql
+++ sqlQuery.sql
@@ -34,3 +34,23 @@ stats sst2 on sst1.GID=sst2.gameid
 where sst2.playerid <> sst1.playerid and sst2.playerid=8701
 group by sst1.playerid,sst2.playerid
 order by cnt desc
+
+
+
+select 
+  sst1.playerid,
+  sst2.playerid as PID, 
+  count (*) as cnt,
+  COUNT(CASE WHEN sst1.win=true THEN 1 END) as wins,
+  COUNT(CASE WHEN sst1.win=false THEN 1 END) as loose,
+  ROUND(CAST(COUNT(CASE WHEN sst1.win=true THEN 1 END) as DECIMAL)/(COUNT(CASE WHEN sst1.win=true THEN 1 END)+COUNT(CASE WHEN sst1.win=false THEN 1 END))*100,2) as winrate from 
+(
+select distinct st1.gameid as GID,st1.playerid as playerid,st1.win as win
+from stats st1
+where st1.playerid = 259
+) sst1
+left outer join 
+stats sst2 on sst1.GID=sst2.gameid
+where sst2.playerid <> sst1.playerid
+group by sst1.playerid,sst2.playerid
+order by cnt desc,winrate desc
